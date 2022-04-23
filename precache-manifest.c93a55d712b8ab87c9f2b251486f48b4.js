4bda28254a9af0bbeb75ee60c4e0b511f6a01b54
Abhinav Mangla <abhinavmangla.india@gmail.com>
2022-04-24 02:31:41 +0530

    Test 1

---
+++ precache-manifest.c93a55d712b8ab87c9f2b251486f48b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f0633632ea6f9d926f05fe29198457",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e799fcba7f02d7139c",
+    "url": "/static/js/2.ec7c55b8.chunk.js"
+  },
+  {
+    "revision": "df11422c31335e0b9f5d",
+    "url": "/static/js/main.7daaf398.chunk.js"
+  },
+  {
+    "revision": "251c36f53bd040f30625",
+    "url": "/static/js/runtime-main.3a327b8f.js"
+  }
+]);
